287768077af85828c1a4bde4f9823f112a5c199b
konstantinos <kostas1295@live.com>
2018-11-29 17:49:09 +0200

    //

---
+++ AccessingData/WorkWithData.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace AccessingData
+{
+    partial class WorkWithData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oleDbDataSetButton = new System.Windows.Forms.Button();
+            this.dataTableButton = new System.Windows.Forms.Button();
+            this.pgeListBox = new System.Windows.Forms.TabPage();
+            this.demoList = new System.Windows.Forms.ListBox();
+            this.sqlDataReaderButton = new System.Windows.Forms.Button();
+            this.sqlDataTableButton = new System.Windows.Forms.Button();
+            this.pgeGrid = new System.Windows.Forms.TabPage();
+            this.demoGrid = new System.Windows.Forms.DataGridView();
+            this.tabDemo = new System.Windows.Forms.TabControl();
+            this.pgeListBox.SuspendLayout();
+            this.pgeGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.demoGrid)).BeginInit();
+            this.tabDemo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // oleDbDataSetButton
+            // 
+            this.oleDbDataSetButton.Location = new System.Drawing.Point(12, 57);
+            this.oleDbDataSetButton.Name = "oleDbDataSetButton";
+            this.oleDbDataSetButton.Size = new System.Drawing.Size(160, 28);
+            this.oleDbDataSetButton.TabIndex = 20;
+            this.oleDbDataSetButton.Text = "Data Set";
+            this.oleDbDataSetButton.Click += new System.EventHandler(this.oleDbDataSetButton_Click);
+            // 
+            // dataTableButton
+            // 
+            this.dataTableButton.Location = new System.Drawing.Point(12, 128);
+            this.dataTableButton.Name = "dataTableButton";
+            this.dataTableButton.Size = new System.Drawing.Size(160, 28);
+            this.dataTableButton.TabIndex = 18;
+            this.dataTableButton.Text = "Create DataTable";
+            this.dataTableButton.Click += new System.EventHandler(this.dataTableButton_Click);
+            // 
+            // pgeListBox
+            // 
+            this.pgeListBox.Controls.Add(this.demoList);
+            this.pgeListBox.Location = new System.Drawing.Point(4, 22);
+            this.pgeListBox.Name = "pgeListBox";
+            this.pgeListBox.Size = new System.Drawing.Size(315, 157);
+            this.pgeListBox.TabIndex = 0;
+            this.pgeListBox.Text = "ListBox";
+            this.pgeListBox.UseVisualStyleBackColor = true;
+            // 
+            // demoList
+            // 
+            this.demoList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.demoList.IntegralHeight = false;
+            this.demoList.Location = new System.Drawing.Point(0, 0);
+            this.demoList.Name = "demoList";
+            this.demoList.Size = new System.Drawing.Size(315, 157);
+            this.demoList.TabIndex = 0;
+            // 
+            // sqlDataReaderButton
+            // 
+            this.sqlDataReaderButton.Location = new System.Drawing.Point(12, 23);
+            this.sqlDataReaderButton.Name = "sqlDataReaderButton";
+            this.sqlDataReaderButton.Size = new System.Drawing.Size(160, 28);
+            this.sqlDataReaderButton.TabIndex = 19;
+            this.sqlDataReaderButton.Text = "Data Reader";
+            this.sqlDataReaderButton.Click += new System.EventHandler(this.sqlDataReaderButton_Click);
+            // 
+            // sqlDataTableButton
+            // 
+            this.sqlDataTableButton.Location = new System.Drawing.Point(12, 94);
+            this.sqlDataTableButton.Name = "sqlDataTableButton";
+            this.sqlDataTableButton.Size = new System.Drawing.Size(160, 28);
+            this.sqlDataTableButton.TabIndex = 21;
+            this.sqlDataTableButton.Text = "Data Table";
+            this.sqlDataTableButton.Click += new System.EventHandler(this.sqlDataSetButton_Click);
+            // 
+            // pgeGrid
+            // 
+            this.pgeGrid.Controls.Add(this.demoGrid);
+            this.pgeGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pgeGrid.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.pgeGrid.Location = new System.Drawing.Point(4, 22);
+            this.pgeGrid.Name = "pgeGrid";
+            this.pgeGrid.Size = new System.Drawing.Size(315, 157);
+            this.pgeGrid.TabIndex = 1;
+            this.pgeGrid.Text = "DataGridView";
+            this.pgeGrid.UseVisualStyleBackColor = true;
+            // 
+            // demoGrid
+            // 
+            this.demoGrid.AllowUserToAddRows = false;
+            this.demoGrid.AllowUserToDeleteRows = false;
+            this.demoGrid.AllowUserToResizeRows = false;
+            this.demoGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.demoGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.demoGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.demoGrid.Location = new System.Drawing.Point(0, 0);
+            this.demoGrid.MultiSelect = false;
+            this.demoGrid.Name = "demoGrid";
+            this.demoGrid.ReadOnly = true;
+            this.demoGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.demoGrid.Size = new System.Drawing.Size(315, 157);
+            this.demoGrid.TabIndex = 0;
+            // 
+            // tabDemo
+            // 
+            this.tabDemo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabDemo.Controls.Add(this.pgeListBox);
+            this.tabDemo.Controls.Add(this.pgeGrid);
+            this.tabDemo.Location = new System.Drawing.Point(195, 23);
+            this.tabDemo.Name = "tabDemo";
+            this.tabDemo.SelectedIndex = 0;
+            this.tabDemo.Size = new System.Drawing.Size(323, 183);
+            this.tabDemo.TabIndex = 22;
+            // 
+            // WorkWithData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 331);
+            this.Controls.Add(this.oleDbDataSetButton);
+            this.Controls.Add(this.dataTableButton);
+            this.Controls.Add(this.sqlDataReaderButton);
+            this.Controls.Add(this.sqlDataTableButton);
+            this.Controls.Add(this.tabDemo);
+            this.Name = "WorkWithData";
+            this.Text = "Form1";
+            this.pgeListBox.ResumeLayout(false);
+            this.pgeGrid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.demoGrid)).EndInit();
+            this.tabDemo.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button oleDbDataSetButton;
+        internal System.Windows.Forms.Button dataTableButton;
+        internal System.Windows.Forms.TabPage pgeListBox;
+        internal System.Windows.Forms.ListBox demoList;
+        internal System.Windows.Forms.Button sqlDataReaderButton;
+        internal System.Windows.Forms.Button sqlDataTableButton;
+        internal System.Windows.Forms.TabPage pgeGrid;
+        internal System.Windows.Forms.DataGridView demoGrid;
+        internal System.Windows.Forms.TabControl tabDemo;
+    }
+}
+
---
+++ AccessingData/WorkWithData.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Data.OleDb;
+using System.IO;
+
+namespace AccessingData
+{
+    public partial class WorkWithData : Form
+    {
+        public WorkWithData()
+        {
+            InitializeComponent();
+        }
+
+        private void PrepareDemo(bool ShowGrid)
+        {
+            PrepareDemo(ShowGrid, pgeListBox);
+        }
+
+        private void PrepareDemo(bool ShowGrid, TabPage SelectedPage)
+        {
+            if (demoList.DataSource == null)
+            {
+                demoList.Items.Clear();
+            }
+            else
+            {
+                demoList.DataSource = null;
+                demoList.DisplayMember = "";
+            }
+
+            demoGrid.Visible = ShowGrid;
+            tabDemo.SelectedTab = SelectedPage;
+        }
+
+
+        private void sqlDataReaderButton_Click(System.Object sender, System.EventArgs e)
+        {
+            PrepareDemo(false);
+            DataReaderFromOleDB();
+        }
+        private void DataReaderFromOleDB()
+        {
+            string strSQL = "SELECT * FROM Customers";
+
+            try
+            {
+
+                using (OleDbConnection cnn = new OleDbConnection(Properties.Settings.Default.OleDbConnectionString))
+                {
+                    using (OleDbCommand cmd = new OleDbCommand(strSQL, cnn))
+                    {
+                        cnn.Open();
+
+                        using (OleDbDataReader dr = cmd.ExecuteReader())
+                        {
+                            // Loop through all the rows, retrieving the 
+                            // columns you need. Also look into the GetString
+                            // method (and other Get... methods) for a faster 
+                            // way to retrieve individual columns.
+                           
+                            while (dr.Read())
+                            {
+                                demoList.Items.Add(string.Format("{0} \t {1} \t {2}", dr["CompanyName"], dr["ContactName"], dr["ContactTitle"]));
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void oleDbDataSetButton_Click(System.Object sender, System.EventArgs e)
+        {
+            PrepareDemo(true);
+            DataSetFromOleDb();
+        }
+        private void DataSetFromOleDb()
+        {
+            string strSQL = "SELECT * FROM Products WHERE CategoryID=1";
+
+            try
+            {
+                using (OleDbDataAdapter adapter = new OleDbDataAdapter(strSQL, Properties.Settings.Default.OleDbConnectionString))
+                {
+
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds, "ProductInfo");
+
+
+                    demoList.DataSource = ds.Tables["ProductInfo"];
+                    demoList.DisplayMember = "ProductName";
+
+                    // Εναλλάκτικά θα μπορούσαμε να γεμίσουμε το Listbox
+                    // παίρνοντας μία μία τις γραμμές του πίνακα και προσθέτοντάς τες
+
+                    //For Each dr As DataRow In _
+                    // ds.Tables("ProductInfo").Rows
+                    //    demoList.Items.Add(dr("ProductName").ToString)
+                    //Next dr
+
+                    // Want to bind a grid? It's this easy:
+                    demoGrid.DataSource = ds.Tables["ProductInfo"];
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void sqlDataSetButton_Click(System.Object sender, System.EventArgs e)
+        {
+            PrepareDemo(true);
+            DataTableFromSQL();
+        }
+        private void DataTableFromSQL()
+        {
+            string strSQL = "SELECT * FROM Products WHERE CategoryID = 1";
+
+            try
+            {
+                using (OleDbDataAdapter adapter = new OleDbDataAdapter(strSQL, Properties.Settings.Default.OleDbConnectionString))
+                {
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+
+                    foreach (DataColumn dc in dt.Columns)
+                    {
+                        demoList.Items.Add(string.Format("{0} ({1})", dc.ColumnName, dc.DataType));
+                    }
+
+                    demoGrid.DataSource = dt;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        private void dataTableButton_Click(System.Object sender, System.EventArgs e)
+        {
+            PrepareDemo(true, pgeGrid);
+            CreateDataTable();
+        }
+        private void CreateDataTable()
+        {
+            // Create a DataTable filled with information
+            // about files in the current folder.
+            // 
+            // Note the use of the FileInfo and 
+            // DirectoryInfo objects, provided by the 
+            // .NET framework, in the System.IO namespace.
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("FileName", typeof(System.String));
+            dt.Columns.Add("Size", typeof(System.Int64));
+
+            DataRow dr = default(DataRow);
+            DirectoryInfo dir = new DirectoryInfo("C:\\");
+            foreach (FileInfo fi in dir.GetFiles())
+            {
+                dr = dt.NewRow();
+                dr[0] = fi.Name;
+                dr[1] = fi.Length;
+                dt.Rows.Add(dr);
+            }
+
+            // Bind the DataGridView to this DataTable.
+            demoGrid.DataSource = dt;
+        }
+    }
+}
